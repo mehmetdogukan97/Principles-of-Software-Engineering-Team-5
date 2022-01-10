4be79d42e0da51c699b094b4d024d8c9a239766e
pinar11 <56875520+pinar11@users.noreply.github.com>
2022-01-11 00:32:15 +0300

    I created strings

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,4 +1,10 @@
 package com.company;
+import java.io.Serializable;
 
-public class Person {
+public class Person implements Serializable {
+    public static String MALE="MALE";
+    public static String FEMALE="FEMALE";
+    public static int MAX_PARENTS=2;
+
+
 }
