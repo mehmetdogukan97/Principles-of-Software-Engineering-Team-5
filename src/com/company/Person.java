82aad9f14fbd1804dc1acd922368529f7f47cff8
pinar11 <56875520+pinar11@users.noreply.github.com>
2022-01-11 01:35:55 +0300

    Created constants like gender names

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,10 +1,23 @@
 package com.company;
 import java.io.Serializable;
-
+import java.util.ArrayList;
 public class Person implements Serializable {
     public static String MALE="MALE";
     public static String FEMALE="FEMALE";
     public static int MAX_PARENTS=2;
+
+    private String fname = "";
+    private String lname = "";
+    private String lnameAfter = "";
+    private String gender = "";
+    private String bio = "";
+    private Person[] parents = new Person[MAX_PARENTS];
+    private ArrayList<Person> children = new ArrayList<Person>();
+    private Person spouse;
+
+
+
+
 
 
 }
