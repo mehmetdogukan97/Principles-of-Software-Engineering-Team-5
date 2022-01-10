5e32249d45bd2e651da679dee622dfe37438054a
Eda Beder <71968528+edabeder@users.noreply.github.com>
2022-01-11 02:26:55 +0300

    Added handleAddRelative method at Controller

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,4 +1,11 @@
 package com.company;
+import javafx.fxml.FXML;
+import java.awt.event.ActionEvent;
 
 public class Controller {
+
+    @FXML
+    private void handleAddRelative(ActionEvent event){
+
+    }
 }
