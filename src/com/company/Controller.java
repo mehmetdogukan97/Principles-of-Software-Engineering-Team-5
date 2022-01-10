08d596ca15f69a3c56b0f05dc412ca6edb43f018
Eda Beder <71968528+edabeder@users.noreply.github.com>
2022-01-11 02:39:57 +0300

    addRelative method created to open a new window for adding relative

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,11 +1,33 @@
 package com.company;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class Controller {
+    @FXML
+    private Button addRelative;
 
     @FXML
-    private void handleAddRelative(ActionEvent event){
-
+    private void handleAddRelative(ActionEvent event) {
+        Parent root;
+        try {
+            Stage stage = (Stage) addRelative.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("newRelative.fxml"));
+            loader.setController(this);
+            root = loader.load();
+            stage = new Stage();
+            stage.setTitle("Add Relative");
+            stage.setScene(new Scene(root, 300, 600));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
