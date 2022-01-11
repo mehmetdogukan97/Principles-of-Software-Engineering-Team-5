b330834dcd54e41130b5cfff23836283041ad694
pinar11 <56875520+pinar11@users.noreply.github.com>
2022-01-11 03:03:40 +0300

    set and get methods are created

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -15,9 +15,72 @@ public class Person implements Serializable {
     private ArrayList<Person> children = new ArrayList<Person>();
     private Person spouse;
 
+    public String getFname() {
+        return fname;
+    }
 
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
 
+    public String getLname() {
+        return lname;
+    }
 
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
 
+    public String getLnameAfter() {
+        return lnameAfter;
+    }
 
+    public void setLnameAfter(String lnameAfter) {
+        this.lnameAfter = lnameAfter;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    public  Person getParent(int index){
+        return parents[index];
+    }
+    public Person[] getParents() {
+        return parents;
+
+    }
+    public void setParents(Person[] parents){
+        this.parents=parents;
+    }
+    public Person getChild(int index) {
+        return children.get(index);
+    }
+
+    public ArrayList<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<Person> children) {
+        this.children = children;
+    }
+
+    public Person getSpouse() {
+        return spouse;
+    }
+
+    public void setSpouse(Person spouse) {
+        this.spouse = spouse;
+    }
 }
