2b61d3f04f5fda1e9b28a39c48ba952b7772fd36
pinar11 <56875520+pinar11@users.noreply.github.com>
2022-01-13 02:30:06 +0300

    Created some methods

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.io.Serializable;
 import java.util.ArrayList;
+
 public class Person implements Serializable {
     public static String MALE="MALE";
     public static String FEMALE="FEMALE";
@@ -11,6 +12,7 @@ public class Person implements Serializable {
     private String lnameAfter = "";
     private String gender = "";
     private String bio = "";
+    private Address address;
     private Person[] parents = new Person[MAX_PARENTS];
     private ArrayList<Person> children = new ArrayList<Person>();
     private Person spouse;
@@ -54,6 +56,18 @@ public class Person implements Serializable {
     public void setBio(String bio) {
         this.bio = bio;
     }
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress (Address address) {
+        this.address = address;
+    }
+
+    public void setAddress(String street,String district, String country, String city) {
+        address = new Address(street, district, city, country);
+    }
+
     public  Person getParent(int index){
         return parents[index];
     }
@@ -83,4 +97,41 @@ public class Person implements Serializable {
     public void setSpouse(Person spouse) {
         this.spouse = spouse;
     }
+
+
+
+    public static boolean isEqual(Person p1, Person p2) {
+        boolean flag = false;
+
+        if ( p1.getFname().equals(p2.getFname()) &&
+                p1.getLname().equals(p2.getLname()) &&
+                p1.getGender().equals(p2.getGender()) &&
+                p1.getSpouse() == p2.getSpouse()) {
+
+            flag = true;
+
+            for (int i = 0; i < p1.children.size(); i++) {
+                if (p1.getChild(i) != p2.getChild(i)) {
+                    flag = false;
+                }
+            }
+
+            for (int i = 0; i < MAX_PARENTS; i++) {
+                if (p1.getParent(i) != p2.getParent(i)) {
+                    flag = false;
+                }
+            }
+
+
+        }
+
+        return flag;
+    }
+
+    @Override
+    public String toString() {
+        return fname;
+    }
 }
+
+
