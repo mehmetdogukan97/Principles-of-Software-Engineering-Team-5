3fe4effebff1a6c04709a4a8a49b4e2e505861d7
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-01-13 03:37:08 +0300

    handleHelp function created

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 import java.io.*;
 import javafx.event.ActionEvent;
@@ -25,6 +26,23 @@ public class Controller {
             stage = new Stage();
             stage.setTitle("Add Relative");
             stage.setScene(new Scene(root, 300, 600));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleHelp(ActionEvent event) {
+        Parent root;
+        try {
+            Stage stage = (Stage) addRelative.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("help.fxml"));
+            loader.setController(this);
+            root = loader.load();
+            stage = new Stage();
+            stage.setTitle("Help ");
+            stage.setScene(new Scene(root, 600, 600));
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
